bf0bd2969383ed923dbf411532a62981bd8919e3
ralle12345 <ralle12345@users.noreply.github.com>
2024-09-30 15:14:44 +0200

    Update nextcloud_scan.py
    - ported to Python 3
    - tested with Zabbix 6.0

--- nextcloud_scan.py
+++ nextcloud_scan.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import argparse
 import requests
@@ -7,13 +7,13 @@ import datetime
 import sys
 
 class NCScan:
-    
+
     API_BASE_URI = 'https://scan.nextcloud.com/api'
 
     def __init__(self, uri, requeueMinutes=1440):
         self.uri = uri
         self.uuid = None
-        self.requeueDelta=datetime.timedelta(minutes=requeueMinutes)
+        self.requeueDelta = datetime.timedelta(minutes=requeueMinutes)
 
     def _post(self, endpoint, **kwargs):
         headers = {'X-CSRF': 'true'}
@@ -25,7 +25,7 @@ class NCScan:
         r = requests.get(self.API_BASE_URI + '/' + endpoint, params=kwargs)
         r.raise_for_status()
         return r
-        
+
     def requestUUID(self):
         response = self._post('queue', url=self.uri)
         self.uuid = response.json()['uuid']
@@ -48,12 +48,13 @@ class NCScan:
 
     def doScan(self):
         result = self.requestResult()
+
+        # Zeitzone extrahieren und ignorieren, da strptime in Python 3 keine %Z mehr unterstützt
         lastScanTimestamp = datetime.datetime.strptime(
-            result['scannedAt']['date']
-                + ' '
-                + result['scannedAt']['timezone'],
-            '%Y-%m-%d %H:%M:%S.%f %Z'
+            result['scannedAt']['date'],
+            '%Y-%m-%d %H:%M:%S.%f'
         )
+        
         scanDelta = datetime.datetime.utcnow() - lastScanTimestamp
         result['secondsSinceScan'] = int(scanDelta.total_seconds())
         if scanDelta >= self.requeueDelta:
@@ -66,42 +67,58 @@ class NCScan:
         result = self.doScan()
 
         result['vulnerabilitiesCount'] = len(result['vulnerabilities'])
-        result['hardeningsMissing'] = sum( 1 for isHardened in result['hardenings'].values() if isHardened == False )
-        result['headersMissing'] = sum( 1 for headerPresent in result['setup']['headers'].values() if headerPresent == False )
+        result['hardeningsMissing'] = sum(1 for isHardened in result['hardenings'].values() if isHardened is False)
+        result['headersMissing'] = sum(1 for headerPresent in result['setup']['headers'].values() if headerPresent is False)
 
-        return json.dumps(result, separators=(',',':'))
+        return json.dumps(result, separators=(',', ':'))
+
+    def getFormattedResultJson(self):
+        result = self.doScan()
+
+        result['vulnerabilitiesCount'] = len(result['vulnerabilities'])
+        result['hardeningsMissing'] = sum(1 for isHardened in result['hardenings'].values() if isHardened is False)
+        result['headersMissing'] = sum(1 for headerPresent in result['setup']['headers'].values() if headerPresent is False)
+
+        # Hier erfolgt die formatierte Ausgabe mit Einrückungen
+        return json.dumps(result, indent=4, separators=(',', ':'))
 
 def main():
     parser = argparse.ArgumentParser(
-                                    description='Run a Nextcloud security scan at scan.nextcloud.org',
-                                    )
+        description='Run a Nextcloud security scan at scan.nextcloud.org',
+    )
     parser.add_argument('hostname',
                         help='Publicly-available hostname of the Nextcloud instance to scan'
-                       )
+                        )
     parser.add_argument('-u', '--uri',
                         required=False,
                         default='/',
                         help='Base URI of the Nextcloud instance (default: "%(default)s")'
-                       )
+                        )
     parser.add_argument('-m', '--minutes',
                         required=False,
                         default=1440,
                         type=int,
                         help='Number of minutes after which to initiate a re-scan (default: %(default)s)'
-                       )
+                        )
     parser.add_argument('-D', '--debug',
                         required=False,
                         default=False,
                         action='store_true'
-                       )
+                        )
+    parser.add_argument('-V', '--verbose',
+                        required=False,
+                        default=False,
+                        action='store_true',
+                        help='Print formatted, human-readable JSON results'
+                        )
     args = parser.parse_args()
 
     if args.debug:
         import logging
-        import httplib as http_client
+        import http.client as http_client  # Updated for Python 3
         http_client.HTTPConnection.debuglevel = 1
 
-        # You must initialize logging, otherwise you'll not see debug output.
+        # Initialize logging
         logging.basicConfig()
         logging.getLogger().setLevel(logging.DEBUG)
         requests_log = logging.getLogger("requests.packages.urllib3")
@@ -111,10 +128,15 @@ def main():
     scanner = NCScan(args.hostname + args.uri, requeueMinutes=args.minutes)
 
     try:
-        print(scanner.getResultJson()) 
+        if args.verbose:
+            # Formatted, human-readable JSON output
+            print(scanner.getFormattedResultJson())
+        else:
+            # Compact JSON output
+            print(scanner.getResultJson())
     except requests.exceptions.RequestException:
-        (name, value, traceback) = sys.exc_info()
-        print("Error: ", name, " (", value, ")")
+        name, value, traceback = sys.exc_info()
+        print(f"Error: {name} ({value})")
         sys.exit(1)
 
     sys.exit(0)
